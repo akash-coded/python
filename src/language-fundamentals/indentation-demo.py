ba9855c14091f0d9afe302c86f75bf1d477f1070
Akash Das <mfs.akash@gmail.com>
2022-09-08 21:45:16 +0530

    added two initial demo programs

---
+++ src/language-fundamentals/indentation-demo.py
@@ -0,0 +1,11 @@
+# define main function to print out something
+def main():
+    i = 1
+    limit = 10
+    while (i <= limit):
+        print(i)
+        i = i + 1
+
+
+# call function main
+main()
