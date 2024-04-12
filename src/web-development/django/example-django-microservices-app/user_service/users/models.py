6d4b63742f04ae44719550210d9a9c95dffb58bf
Akash Das <mfs.akash@gmail.com>
2024-04-12 14:02:15 +0530

    added a demo microservice app using django

---
+++ src/web-development/django/example-django-microservices-app/user_service/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
